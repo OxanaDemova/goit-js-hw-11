3e37807c09a9b34a822e7e9755241efdd726fffd
OxanaDemova <93287016+OxanaDemova@users.noreply.github.com>
2022-06-08 00:18:15 +0300

    +formStyle

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-// import './sass/main.scss';
+import './sass/main.scss';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import cardMarckup from "./js/cardMarckup";
